86d9038ef98d2efe03ebe4c5062f0be998f079b2
aran <test@info.com>
2024-09-03 18:09:37 +0200

    xattr Script geplant

--- build_mac_app.sh
+++ build_mac_app.sh
@@ -5,3 +5,7 @@
 chmod +x soft-ui-updater-mac.sh
 mkdir -p soft-ui-updater-mac.app
 cp -f soft-ui-updater-mac.sh soft-ui-updater-mac.app/soft-ui-updater-mac
+
+# chmod +x fix_mac_xattr.sh
+# mkdir -p fix_mac_xattr.app
+# cp -f fix_mac_xattr.sh fix_mac_xattr.app/fix_mac_xattr
